b492d1a4e4e544140a8bc7fc11c99918c16300fb
LuftWaffeTheDumbass <96132323+LuftWaf@users.noreply.github.com>
2025-04-28 21:47:08 +0300

    Update battle1.py

--- main_game/battle_assets/battle1.py
+++ main_game/battle_assets/battle1.py
@@ -337,12 +337,12 @@ def battle():
         # Check if enemy is defeated
         if enemy_HP <= 0:
             fade_to_black(screen, width, height)
+            if player_data["completed_stages"] < 1:
+                with open('player_data.json', 'w') as json_file:
+                    player_data["completed_stages"] += 1
+                    json.dump(player_data, json_file)
             pygame.quit()
             subprocess.run(["python", "main.py"])
-            if player_data["completed_stage_amount"] < 1:
-                with open('player_data.json', 'w') as json_file:
-                    player_data["completed_stage_amount"] += 1
-                    json.dump(player_data, json_file)
             sys.exit()
         
         pygame.display.flip()
